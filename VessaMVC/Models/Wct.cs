d725b4fa024536c1647c9a6d11407f847c248897
SaskaYl <56820603+SaskaYl@users.noreply.github.com>
2019-11-19 13:42:35 +0200

    mvc, vessakontrolleri,modelsit

---
+++ VessaMVC/Models/Wct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace VessaMVC.Models
+{
+    public class Wct
+    {
+        
+
+        public int WcId { get; set; }
+        public string Nimi { get; set; }
+        public string Kaupunki { get; set; }
+        public string Katuosoite { get; set; }
+        public string Postinro { get; set; }
+        public bool? Unisex { get; set; }
+        public bool? Saavutettava { get; set; }
+        public bool Ilmainen { get; set; }
+        public string Aukioloajat { get; set; }
+        public string Koodi { get; set; }
+        public string Ohjeet { get; set; }
+        public DateTime Lisätty { get; set; }
+        public DateTime? Muokattu { get; set; }
+        public int? KäyttäjäId { get; set; }
+       
+
+    }
+}
