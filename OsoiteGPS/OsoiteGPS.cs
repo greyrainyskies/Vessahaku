06fb1efda4e56aa4d5574d267e56f19902343d91
Ville Matias <greyrainyskies@users.noreply.github.com>
2019-11-19 13:41:33 +0200

    Virhekäsittelyä osoitehakuun.

--- OsoiteGPS/OsoiteGPS.cs
+++ OsoiteGPS/OsoiteGPS.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Web;
 using NetTopologySuite.Geometries;
@@ -16,10 +17,30 @@ namespace OsoiteGPS
             {
                 client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
                 var response = client.GetAsync(baseURL + "search?text=" + HttpUtility.UrlEncode(hakutekijät)).Result;
-                var sijainti = JsonConvert.DeserializeObject<Result>(response.Content.ReadAsStringAsync().Result).Features.First();
-                var lon = sijainti.Geometry.Coordinates.First();
-                var lat = sijainti.Geometry.Coordinates.Last();
-                return new Point(lon, lat) { SRID = 4326 };
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var sijainti = JsonConvert.DeserializeObject<Result>(response.Content.ReadAsStringAsync().Result).Features.First();
+                    var lon = sijainti.Geometry.Coordinates.First();
+                    var lat = sijainti.Geometry.Coordinates.Last();
+                    return new Point(lon, lat) { SRID = 4326 };
+                }
+                else
+                {
+                    throw new ArgumentException("Virheelliset hakutekijät. Yritä uudelleen.");
+                }
+
+            }
+        }
+
+        public static Point Haku(string katuosoite, string postinumero, string kaupunki)
+        {
+            try
+            {
+                return Haku($"{katuosoite}, {postinumero} {kaupunki}");
+            }
+            catch (ArgumentException)
+            {
+                throw new ArgumentException("Osoitetieto virheellinen. Yritä uudelleen.");
             }
         }
     }
