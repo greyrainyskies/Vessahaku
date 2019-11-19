c1813c3dd0701637c19a93e2cec68022dace7389
SaskaYl <56820603+SaskaYl@users.noreply.github.com>
2019-11-19 09:24:25 +0200

    Add project files.

---
+++ VessahakuAPI/Models/Wct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using NetTopologySuite.Geometries;
+
+namespace VessahakuAPI.Models
+{
+    public partial class Wct
+    {
+        public Wct()
+        {
+            Kommentit = new HashSet<Kommentit>();
+        }
+
+        public int WcId { get; set; }
+        public string Nimi { get; set; }
+        public string Kaupunki { get; set; }
+        public string Katuosoite { get; set; }
+        public string Postinro { get; set; }
+        public bool? Unisex { get; set; }
+        public bool? Saavutettava { get; set; }
+        public bool Ilmainen { get; set; }
+        public string Aukioloajat { get; set; }
+        public string Koodi { get; set; }
+        public string Ohjeet { get; set; }
+        public DateTime Lisätty { get; set; }
+        public DateTime? Muokattu { get; set; }
+        public int? KäyttäjäId { get; set; }
+        public Geometry Sijainti { get; set; }
+
+        public virtual ICollection<Kommentit> Kommentit { get; set; }
+    }
+}
