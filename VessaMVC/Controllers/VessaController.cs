0fb2f698f45c5843d3c525015e8381bb5e9205ad
SaskaYl <56820603+SaskaYl@users.noreply.github.com>
2019-11-20 14:41:16 +0200

    edit toimii, takaisin-button lisätty detaileihin

--- VessaMVC/Controllers/VessaController.cs
+++ VessaMVC/Controllers/VessaController.cs
@@ -127,7 +127,7 @@ namespace VessaMVC.Controllers
                 
                 if(response.IsSuccessStatusCode)
                 {
-                    return RedirectToAction("Details", id);
+                    return RedirectToAction("Details", new {id=id });
                 }
                 else
                 {
@@ -136,27 +136,6 @@ namespace VessaMVC.Controllers
                 }
             }
         }
-        // GET: Vessa/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Vessa/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+    
     }
 }
