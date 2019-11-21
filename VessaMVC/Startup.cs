33613a194c348a442427ac70c0effb3c6332c907
Marjaana Heikkinen <piritamarjaana@gmail.com>
2019-11-21 14:50:47 +0200

    Bootstrap ulkoasu lisätty yms siihen liittyvää muokkausta

--- VessaMVC/Startup.cs
+++ VessaMVC/Startup.cs
@@ -50,7 +50,7 @@ namespace VessaMVC
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Vessa}/{action=Lahimmat}/{id?}");
             });
         }
     }
